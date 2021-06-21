d9e044ffea4d4942e6f771d2f396641da658dde2
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-06-21 21:48:43 +0800

    Added Prints

--- databases/mongodbrepo/readers.go
+++ databases/mongodbrepo/readers.go
@@ -10,14 +10,14 @@ import (
 
 // TODO: implement filters
 func (db *MongoDBRepo) Fetch(filter interface{}, collectionName string) interface{} {
-
+	fmt.Println("This calls the fetcher")
 	godotenv.Load()
 	configs, _ := godotenv.Read()
 	collection := db.Client.Database(configs["MONGO_DATABASE"]).Collection(collectionName)
 
 	result, err := collection.Find(context.TODO(), filter)
 	if err != nil {
-		fmt.Println("Problem in inserting data to mongodb")
+		fmt.Println("Problem fetching data to mongodb")
 		log.Fatal(err)
 	}
 
