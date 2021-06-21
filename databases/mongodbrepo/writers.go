9047e11aecfa00d5e7e737553f0c1edcd24b757d
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-06-21 16:28:49 +0800

    Organized mongodb files

---
+++ databases/mongodbrepo/writers.go
@@ -0,0 +1,27 @@
+package mongodbrepo
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+/**
+ * Executes a create call on mongo and returns a resulting interface
+ */
+func (db *MongoDBRepo) Create(inputs interface{}, collectionName string) interface{} {
+
+	godotenv.Load()
+	configs, _ := godotenv.Read()
+	collection := db.Client.Database(configs["MONGO_DATABASE"]).Collection(collectionName)
+
+	result, err := collection.InsertOne(context.TODO(), inputs)
+	if err != nil {
+		fmt.Println("Problem in inserting data to mongodb")
+		log.Fatal(err)
+	}
+
+	return result
+}
