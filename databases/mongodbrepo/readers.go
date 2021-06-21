a1547d012b4df887a29360b9797bd8f3091e9605
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-06-21 21:16:34 +0800

    Implemented readers

--- databases/mongodbrepo/readers.go
+++ databases/mongodbrepo/readers.go
@@ -1,2 +1,25 @@
 package mongodbrepo
 
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+// TODO: implement filters
+func (db *MongoDBRepo) Fetch(filter interface{}, collectionName string) interface{} {
+
+	godotenv.Load()
+	configs, _ := godotenv.Read()
+	collection := db.Client.Database(configs["MONGO_DATABASE"]).Collection(collectionName)
+
+	result, err := collection.Find(context.TODO(), filter)
+	if err != nil {
+		fmt.Println("Problem in inserting data to mongodb")
+		log.Fatal(err)
+	}
+
+	return result
+}
