3533f270b1e9d1966c5a9a4e979d8e8f54a24a9b
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-06-21 21:26:23 +0800

    Re-added fetching of client

--- databases/mongodbrepo/common.go
+++ databases/mongodbrepo/common.go
@@ -1,11 +1,14 @@
 package mongodbrepo
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"sync"
 
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var clientInstance *mongo.Client
@@ -14,6 +17,34 @@ var mongoOnce sync.Once
 
 type MongoDBRepo struct {
 	Client *mongo.Client
+}
+
+/**
+ * Returns the mongo db client for use
+ */
+func GetClient() (*mongo.Client, error) {
+	godotenv.Load()
+	configs, _ := godotenv.Read()
+
+	mongoOnce.Do(func() {
+		// Setting client optons
+		clientOptions := options.Client().ApplyURI(configs["MONGO_URL"])
+
+		client, err := mongo.Connect(context.TODO(), clientOptions)
+		if err != nil {
+			log.Fatal(err)
+			clientInstanceError = err
+		}
+
+		err = client.Ping(context.TODO(), nil)
+		if err != nil {
+			clientInstanceError = err
+		}
+
+		clientInstance = client
+	})
+
+	return clientInstance, clientInstanceError
 }
 
 /**
